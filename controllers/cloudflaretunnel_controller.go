e0b4a7f46682e6d4bd720b28d3ff158eae5b9dad
Sayak Mukhopadhyay <sayak@beezlabs.com>
2022-06-18 15:10:02 +0530

    feat: add preliminary logic to reconcile

--- controllers/cloudflaretunnel_controller.go
+++ controllers/cloudflaretunnel_controller.go
@@ -18,11 +18,18 @@ package controllers
 
 import (
 	"context"
+	"crypto/rand"
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/cloudflare/cloudflare-go"
 
 	cloudflaretunneloperatorv1alpha1 "github.com/beezlabs-org/cloudflare-tunnel-operator/api/v1alpha1"
 )
@@ -37,19 +44,92 @@ type CloudflareTunnelReconciler struct {
 //+kubebuilder:rbac:groups=cloudflare-tunnel-operator.beezlabs.app,resources=cloudflaretunnels/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cloudflare-tunnel-operator.beezlabs.app,resources=cloudflaretunnels/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the CloudflareTunnel object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.2/pkg/reconcile
 func (r *CloudflareTunnelReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	lfc := log.FromContext(ctx)
+	lfc.Info("Starting Reconciler...")
 
-	// TODO(user): your logic here
+	var cloudflareTunnel cloudflaretunneloperatorv1alpha1.CloudflareTunnel
+	if err := r.Get(ctx, req.NamespacedName, &cloudflareTunnel); err != nil {
+		lfc.Error(err, "could not fetch CloudflareTunnel")
+		return ctrl.Result{}, err
+	}
+
+	name := cloudflareTunnel.Name
+	namespace := cloudflareTunnel.Namespace
+	spec := cloudflareTunnel.Spec
+
+	secretName := spec.CredentialSecretName
+
+	if len(secretName) == 0 {
+		err := fmt.Errorf("CredentialSecretName key does not exist")
+		lfc.Error(err, "CredentialSecretName not found")
+		return ctrl.Result{}, err
+	}
+
+	var secret v1.Secret
+	if err := r.Get(ctx, types.NamespacedName{Name: secretName, Namespace: namespace}, &secret); err != nil {
+		if errors.IsNotFound(err) {
+			lfc.Error(err, "could not find secret with name", secretName)
+		}
+		return ctrl.Result{}, err
+	}
+
+	encodedCredentials, ok := secret.Data["credentials"]
+
+	if !ok {
+		err := fmt.Errorf("invalid key")
+		lfc.Error(err, "key credentials not found")
+		return ctrl.Result{}, err
+	}
+
+	credentials := string(encodedCredentials)
+
+	cf, err := cloudflare.NewWithAPIToken(credentials)
+	if err != nil {
+		lfc.Error(err, "could not create cloudflare instance")
+	}
+
+	tunnelSecret, err := generateTunnelSecret()
+	if err != nil {
+		lfc.Error(err, "could not generate tunnel secret")
+	}
+
+	tunnelParams := cloudflare.TunnelCreateParams{
+		AccountID: cf.AccountID,
+		Name:      name,
+		Secret:    tunnelSecret,
+	}
+
+	falsePointer := false
+
+	tunnels, err := cf.Tunnels(ctx, cloudflare.TunnelListParams{AccountID: cf.AccountID, Name: name, IsDeleted: &falsePointer})
+	if err != nil {
+		lfc.Error(err, "could not fetch tunnel list")
+	}
+
+	var tunnel cloudflare.Tunnel
+
+	if len(tunnels) >= 2 {
+		lfc.Error(err, "2 or more tunnels already exists with the given name. Unable to choose between one of them")
+	} else if len(tunnels) == 1 {
+		lfc.Info("Tunnel already exists. Reconciling...")
+		tunnel = tunnels[0]
+	} else {
+		lfc.Info("Tunnel doesn't exist. Creating...")
+		tunnel, err = cf.CreateTunnel(ctx, tunnelParams)
+		if err != nil {
+			lfc.Error(err, "could not create the tunnel")
+		}
+	}
+
+	var pod v1.Pod
+	if err := r.Get(ctx, types.NamespacedName{Name: name, Namespace: namespace}, pod); err != nil {
+		if errors.IsNotFound(err) {
+			lfc.Info("creating pod...")
+			err = r.Create(ctx, pod)
+		}
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -59,4 +139,10 @@ func (r *CloudflareTunnelReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&cloudflaretunneloperatorv1alpha1.CloudflareTunnel{}).
 		Complete(r)
+}
+
+func generateTunnelSecret() (string, error) {
+	randomBytes := make([]byte, 32)
+	_, err := rand.Read(randomBytes)
+	return string(randomBytes), err
 }
--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cloudflare/cloudflare-go v0.41.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/form3tech-oss/jwt-go v3.2.3+incompatible // indirect
@@ -30,6 +31,7 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
@@ -49,19 +51,19 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
-	golang.org/x/sys v0.0.0-20211029165221-6e7872819dc8 // indirect
-	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/api v0.23.5 // indirect
 	k8s.io/apiextensions-apiserver v0.23.5 // indirect
 	k8s.io/component-base v0.23.5 // indirect
